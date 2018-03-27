aa8cb30efebf6d7c3eaab02e48919632356f7dd5
Brad Howes <b.howes@criteo.com>
2018-03-27 12:39:37 +0200

    Now run unit tests right after the lib they test is built

--- CMakeStuff/UnitTests.cmake
+++ CMakeStuff/UnitTests.cmake
@@ -54,16 +54,80 @@ macro(ADD_UNIT_TEST FILE)
     # the unit test succeeds, we 'touch' a sentinal file that lets the CMake build know that everything worked.
     #
     add_custom_command(OUTPUT "${aut_OK}"
-                       COMMAND bash
-                       "${PROJECT_SOURCE_DIR}/Scripts/dotest.sh"
-                       "${CMAKE_CURRENT_BINARY_DIR}/${aut_NAME}" "${aut_LOG}" "${aut_OK}"
+                       COMMAND ${CMAKE_COMMAND} -E remove ${aut_OK} ${aut_LOG}
+                       COMMAND ${aut_NAME} > ${aut_LOG} 2>&1 || (cat ${aut_LOG} && false)
+                       COMMAND ${CMAKE_COMMAND} -E touch "${aut_OK}"
                        BYPRODUCTS "${aut_LOG}"
-                       DEPENDS "${aut_NAME}" ${PROJECT_SOURCE_DIR}/Scripts/dotest.sh
+                       DEPENDS ${aut_NAME}
                        COMMENT "Running unit tests in ${aut_NAME}" )
 
     # Create a custom target that will execute the above custom command whenever necessary to regenerate the
     # test output file
     #
-    add_custom_target(Run${aut_NAME} ALL DEPENDS ${aut_OK} ${aut_NAME})
+    add_custom_target(${aut_NAME}Passed DEPENDS ${aut_OK} ${aut_NAME} COMMENT "${aut_NAME} PASSED")
 
 endmacro(ADD_UNIT_TEST)
+
+macro(add_tested_library NAME)
+
+    if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}")
+        file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
+    endif()
+
+    set(atl_TESTED ${NAME})
+    set(atl_UNTESTED "${NAME}_tests")
+    set(atl_OBJS "${NAME}_objs")
+    set(atl_SRCS)
+    set(atl_DEPS)
+    set(atl_TESTS)
+
+    set(atl_TAG atl_SRCS)
+    foreach(atl_ARG ${ARGN})
+        if("${atl_ARG}" STREQUAL "SOURCES")
+            set(atl_TAG atl_SRCS)
+        elseif("${atl_ARG}" STREQUAL "DEPS")
+            set(atl_TAG atl_DEPS)
+        elseif("${atl_ARG}" STREQUAL "TEST")
+            set(atl_TAG atl_TESTS)
+            list(APPEND ${atl_TAG} "TEST")
+        else()
+            list(APPEND ${atl_TAG} ${atl_ARG})
+        endif()
+    endforeach()
+
+    # message("${atl_UNTESTED} sources: ${atl_SRCS}")
+    # message("${atl_UNTESTED} deps: ${atl_DEPS}")
+
+    add_library(${atl_OBJS} OBJECT ${atl_SRCS})
+    add_library(${atl_UNTESTED} SHARED $<TARGET_OBJECTS:${atl_OBJS}>)
+    target_link_libraries(${atl_UNTESTED} ${atl_DEPS})
+
+    set(atl_TEST_TARGETS)
+    set(atl_TST)
+
+    foreach(atl_ARG ${atl_TESTS})
+        if("${atl_ARG}" STREQUAL "TEST")
+            if(atl_TST)
+                add_unit_test(${atl_TST} ${atl_UNTESTED})
+                list(APPEND atl_TEST_TARGETS ${aut_NAME}Passed)
+            endif()
+            set(atl_TST)
+        else()
+            list(APPEND atl_TST ${atl_ARG})
+        endif()
+    endforeach()
+
+    if(atl_TST)
+        add_unit_test(${atl_TST} ${atl_UNTESTED})
+        list(APPEND atl_TEST_TARGETS ${aut_NAME}Passed)
+    endif()
+
+    # message("${atl_UNTESTED} tests: ${atl_TEST_TARGETS}")
+
+    add_library(${atl_TESTED} SHARED $<TARGET_OBJECTS:${atl_OBJS}>)
+    target_link_libraries(${atl_TESTED} ${atl_DEPS})
+    set_target_properties(${atl_TESTED} PROPERTIES VERSION ${SIDECAR_VERSION} SOVERSION ${SIDECAR_VERSION})
+
+    add_dependencies(${atl_TESTED} ${atl_TEST_TARGETS})
+
+endmacro(add_tested_library)
